c2e99fb8ef9eb570bf85fb17dc64f24156598a0d
Ryan Kerrigan <ryan.kerrigan22@gmail.com>
2024-04-01 14:36:28 -0500

    deleted zone identifiers

--- calculator.js
+++ calculator.js
@@ -17,30 +17,40 @@ function getCurrentUIValues() {
   }
 }
 
-// GOAL IS TO MAKE LOAN CALCULATOR FUNCTIONAL. CALC TAKES AN AMOUNT TO LOAN (PRINCIPLE), A TERM IN YEARS, AND A YEARLY RATE.
-// THE OUTPUT SHOULD BE THE MONTHLY PAYMENT OF THE LOAN.
-// CALCULATE USING FORMULA:  MONTHLY PAYMENT = P * i1 - (1 + i)-n
-// P = AMOUNT OF PRINCIPLE i = PERIODIC INTEREST RATE (YEARLY RATE / 12) n = TOTAL NUMBER OF PAYMENTS (YEARS * 12)
-// GOAL IS TO WRITE TESTS FOR CALCULATE MONTHLY PAYMENT FUNCTION IN calculator-test.js
-
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const values = {amount: 10000, years: 10, rate: 4.5};
+  const amountUI = document.getElementById('loan-amount');
+  amountUI.value = values.amount;
+  const yearsUI = document.getElementById('loan-years');
+  yearsUI = values.years;
+  const rateUI = document.getElementById('loan-rate');
+  rateUI.value = values.rate;
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const currentUIValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentUIValues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const monthlyRate = (values.rate / 100) / 12;
+  const n = Math.floor(values.years * 12);
+  return (
+    (monthlyRate * values.amount) / (1 - Math.pow((1 + monthlyRate), -n))).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyUI = document.getElementById('monthly-payment');
+  monthlyUI.innerText = '$' + monthly;
 }
